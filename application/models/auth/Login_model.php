4192f4d8f574c52a6893d3c6293fc8fab0e89dd3
CesarDarryl <darilmikail@gmail.com>
2020-11-09 19:44:29 +0700

    Commiting :

--- application/models/auth/Login_model.php
+++ application/models/auth/Login_model.php
@@ -1,65 +1,59 @@
 <?php
-    defined('BASEPATH') OR exit('no direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-    //login model
-    class Login_model extends CI_MODEL
+class Login_model extends CI_Model {
+    protected $username;
+    protected $password;
+
+    public function __construct()
     {
-        protected $username;
-        protected $password;
-
-        
-        public function __construct()
-        {
-            parent::__construct();            
-        }
-
-        //function login
-        public function login($username = '', $password = '')
-        {
-            $this->username = $username;
-            $this->password = $password;
-        }
-
-        //function user exist or Not
-        public function is_user_exist()
-        {
-            $username = $this->username;
-
-            $check = $this->db
-                ->where('username',$username)
-                ->get('users')
-                ->num_rows();
-            return($check > 0) ? TRUE : FALSE;
-        }
-
-        //get username from database
-        protected function _get($row = '')
-        {
-            $username = $this->username;
-
-            $field = $this->db
-                ->select($row)
-                ->where('username',$username)
-                ->get('users')
-                ->row()
-                ->$row;
-            return $field;
-        }
-
-        public function get_role()
-        {
-            return $this->_get('role_id');
-        }
-    
-        public function get_password()
-        {
-            return $this->_get('password');
-        }
-    
-        public function logged_user_id()
-        {
-            return $this->_get('id');
-        }
-        
+        parent::__construct();
     }
-?>
+
+    public function login($username = '', $password = '')
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function is_user_exist()
+    {
+        $username = $this->username;
+
+        $check = $this->db
+            ->where('username', $username)
+            ->get('users')
+            ->num_rows();
+
+        return ($check > 0) ? TRUE : FALSE;
+    }
+
+    protected function _get($row = '')
+    {
+        $username = $this->username;
+
+        $field = $this->db
+            ->select($row)
+            ->where('username', $username)
+            ->get('users')
+            ->row()
+            ->$row;
+
+        return $field;
+    }
+
+    public function get_role()
+    {
+        return $this->_get('role_id');
+    }
+
+    public function get_password()
+    {
+        return $this->_get('password');
+    }
+
+    public function logged_user_id()
+    {
+        return $this->_get('id');
+    }
+}
